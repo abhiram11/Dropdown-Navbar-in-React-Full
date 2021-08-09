5553ef91accba003f5b0fb21a3e820c5ccbe8d5c
abhiram11 <abhiramsatpute@gmail.com>
2021-08-09 08:17:36 +0530

    made a json with title, path, cName, connecting n map it to Dropdown-js'

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -4,7 +4,32 @@ import { MenuItems } from "./MenuItems";
 import "./Dropdown.css";
 
 function Dropdown() {
-  return <></>;
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  return (
+    <>
+      <ul
+        onClick={handleClick}
+        className={click ? "dropdown-menu clicked" : "dropdown-menu"}
+      >
+        {MenuItems.map((item, index) => {
+          return (
+            <li key={index}>
+              {/* taking cName from MenuItmes dict */}
+              <Link
+                className={item.cName}
+                to={item.path}
+                onClick={() => setClick(false)}
+              >
+                {item.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
 
 export default Dropdown;
